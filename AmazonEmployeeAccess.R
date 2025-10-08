36fba2cd00b6e75a6e99187aa22a36621fff6759
Sofia Scribner <sofiadscribner@gmail.com>
2025-10-08 12:19:33 -0600

    preliminary logistic regression practice

--- AmazonEmployeeAccess.R
+++ AmazonEmployeeAccess.R
@@ -4,6 +4,7 @@ library(tidyverse)
 library(tidymodels)
 library(vroom)
 library(ggmosaic)
+library(embed)
 
 # read in data
 
@@ -59,5 +60,37 @@ practice_recipe <- recipe(ACTION ~., data = train) %>%
 prep <- prep(practice_recipe)
 baked <- bake(prep, new_data = train)
   
+# LOGISTIC REGRESSION
 
+# recipe
 
+lencode_recipe <- recipe(ACTION ~., data = train) %>%
+  step_other(all_nominal_predictors(), threshold = .001) %>%
+  step_lencode_mixed(all_nominal_predictors(), outcome = vars(ACTION))
+
+# model
+
+logRegModel <- logistic_reg() %>%
+  set_engine("glm")
+
+# workflow
+
+logReg_workflow <- workflow() %>%
+  add_recipe(lencode_recipe) %>%
+  add_model(logRegModel) %>%
+  fit(data = train)
+
+# make preds
+
+logReg_preds <- predict(logReg_workflow,
+                        new_data = test,
+                        type = "prob")
+
+# format for kaggle submission
+
+logRegsub <- bind_cols(test %>% select(id), logReg_preds %>% select(.pred_1)) %>%
+  rename(ACTION = .pred_1)
+
+# save predictions locally
+
+vroom_write(x=logRegsub, file="./logRegPreds.csv", delim=",")
